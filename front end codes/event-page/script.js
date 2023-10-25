e55ea28be4c2602fbd01e3878dc585f003ab1f3e
kishan <kishankmr8078@gmail.com>
2023-10-25 09:56:30 +0530

    loginWithProfile has been updated(blue-background)

---
+++ front end codes/event-page/script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelector('.nav-links');
+let menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', function(){
+  menuBtn.classList.toggle('active');
+  navLinks.classList.toggle('active');
+})
