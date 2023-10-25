ddd057e52600dd132ec017a29d1cfc9ad31d1506
Thilak Acharya <142791454+Thilak19@users.noreply.github.com>
2023-10-25 12:08:21 +0000

    added page number

--- front end codes/alumni-section/script.js
+++ front end codes/alumni-section/script.js
@@ -4,4 +4,34 @@ let menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', function(){
   menuBtn.classList.toggle('active');
   navLinks.classList.toggle('active');
-})
+});
+
+
+let count = 0;
+let btn = document.querySelectorAll(".fa-solid");
+
+let boxElement = document.querySelectorAll(".box");
+btn[1].onclick = function next()
+{
+  if(count<4)
+    {
+     for(let i=0;i<boxElement.length;i++)
+    {
+       boxElement[count].style.background= "lightgray";
+    }
+  count++;
+  boxElement[count].style.background = "yellow"; 
+    }
+};
+btn[0].onclick = function next()
+{
+  if(count>0)
+    {
+     for(let i=0;i<boxElement.length;i++)
+    {
+       boxElement[count].style.background= "lightgray"
+    }
+  count--;
+  boxElement[count].style.background = "yellow"; 
+    }
+};
